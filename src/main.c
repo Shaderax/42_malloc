12154be7f506bfc9ebcf86f7fe7f92b99847822f
Nathan ROUZEVAL <nrouzeva@e1r4p2.42.fr>
2018-05-09 18:58:23 +0200

    OUI

--- src/main.c
+++ src/main.c
@@ -2,16 +2,39 @@
 #include <stdio.h>
 #include "malloc.h"
 
+void	ft_show_alloc()
+{
+	t_page *tiny;
+	t_block	*cur;
+
+	tiny = g_maloc.tiny;
+	cur = (void *)(tiny + sizeof(t_page));
+	while (1)
+	{
+		if (cur->size == 0 || cur == (void *)(tiny + TINY_MAP))
+		{
+			if (!tiny->next)
+				break ;
+			tiny = tiny->next;
+		}
+		printf("Size : %d, Use : %d\n", cur->size, (cur->use & (1 << 1)) ? 1 : 0);
+		cur = (void*)(cur) + sizeof(t_block) + cur->size;
+	}
+}
+
 int main(int argc, char **argv)
 {
 	char *str;
 	char *str2;
+	char *str3;
+	char *str4;
+	int i = 0;
 
 	printf("Size of page : %d\n", getpagesize());
-	str = ft_malloc(20);
-	str[0] = 'h';
-	str2 = ft_malloc(20);
-	str2[0] = 'o';
-	printf("%s\n", str2);
-	printf("%s\n", str);
+	while (i++ < 24)
+	{
+		str = ft_malloc(300);
+		str[0] = 'h';
+	}
+	ft_show_alloc();
 }
--- src/malloc.c
+++ src/malloc.c
@@ -6,7 +6,7 @@
 /*   By: nrouzeva <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/05/08 13:00:26 by nrouzeva          #+#    #+#             */
-/*   Updated: 2018/05/08 16:22:28 by nrouzeva         ###   ########.fr       */
+/*   Updated: 2018/05/09 15:59:13 by nrouzeva         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,23 +23,29 @@ void	*alloc_tiny(size)
 		g_maloc.tiny = mmap(NULL, TINY_MAP, PROT_READ | PROT_WRITE,
 			   	MAP_ANON | MAP_PRIVATE, -1, 0);
 	cur_page = g_maloc.tiny;
-	cur = (void *)(g_maloc.tiny + sizeof(t_page));
-	while (cur != (void *)(g_maloc.tiny + TINY_MAP) && cur->use && cur->size && cur->size >= size && (void*)(g_maloc.tiny + TINY_MAP) - (void *)cur > size + sizeof(t_block))
-		cur = cur + sizeof(t_block) + cur->size;
-	if (cur != (void *)(g_maloc.tiny + TINY_MAP))
+	while (1)
 	{
+		cur = (void *)(cur_page + sizeof(t_page));
+		while ((cur != (void *)(cur_page) + TINY_MAP) && (cur->use && (cur->size < size || (void*)(cur_page) + TINY_MAP - (void *)(cur) > size + sizeof(t_block))))
+			cur = (void*)(cur) + sizeof(t_block) + cur->size;
+		if (cur == (void *)(cur_page + TINY_MAP) || (void*)(cur_page + TINY_MAP) - (void *)cur < size + sizeof(t_block))
+		{
+			if (!cur_page->next)
+				cur_page->next = mmap(NULL, TINY_MAP, PROT_READ | PROT_WRITE,
+					   	MAP_ANON | MAP_PRIVATE, -1, 0);
+			cur_page = cur_page->next;
+			continue ;
+		}
 		cur->size = size;
 		cur->use = 1;
+		return (((void *)cur) + sizeof(t_block));
 	}
-	else if (cur_page->next)
-	{
-
-	}
-	return cur + sizeof(t_block);
 }
 
 void	*ft_malloc(size_t size)
 {
+	if (size <= 0)
+		return (NULL);
 	if (size <= MAX_TINY)
 		return (alloc_tiny(size));
 //	else if (size <= MAX_SMALL)
